5c993f6ea4a1fd1a85727b90a85be4ea71d11a99
Daniel Rueda <78886794+DaniloRF22@users.noreply.github.com>
2021-04-14 16:24:19 -0600

    ReportViewer

--- BazarRoxana/FacturaVentas.Designer.vb
+++ BazarRoxana/FacturaVentas.Designer.vb
@@ -22,16 +22,30 @@ Partial Class FacturaVentas
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer()
         Me.SuspendLayout()
+        '
+        'ReportViewer1
+        '
+        Me.ReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ReportViewer1.Location = New System.Drawing.Point(0, 0)
+        Me.ReportViewer1.Name = "ReportViewer1"
+        Me.ReportViewer1.ServerReport.BearerToken = Nothing
+        Me.ReportViewer1.Size = New System.Drawing.Size(1067, 554)
+        Me.ReportViewer1.TabIndex = 0
         '
         'FacturaVentas
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1067, 554)
+        Me.Controls.Add(Me.ReportViewer1)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "FacturaVentas"
         Me.Text = "FacturaVentas"
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents ReportViewer1 As Microsoft.Reporting.WinForms.ReportViewer
 End Class
--- BazarRoxana/FacturaVentas.vb
+++ BazarRoxana/FacturaVentas.vb
@@ -1,5 +1,6 @@
 ﻿Public Class FacturaVentas
     Private Sub FacturaVentas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+        Me.ReportViewer1.RefreshReport
     End Sub
 End Class
