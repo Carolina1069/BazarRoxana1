1b57450387194d1ebeb38ecf7501ce49231b6a59
Carlos Amaya <carlosdaniel1899@gmail.com>
2021-07-21 21:16:38 -0600

    AAAAAAAAaaaaaaaa

---
+++ BazarRoxana/MiEmpresa.Designer.vb
@@ -0,0 +1,247 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MiEmpresa
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.txtcod = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.btndel = New System.Windows.Forms.Button()
+        Me.btnsave = New System.Windows.Forms.Button()
+        Me.txtTelefono = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtCAI = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtRTN = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btncan = New System.Windows.Forms.Button()
+        Me.txtDireccion = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TxtEmail = New System.Windows.Forms.TextBox()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(456, 305)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(128, 13)
+        Me.Label7.TabIndex = 34
+        Me.Label7.Text = "Selecciona una fotografia"
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Location = New System.Drawing.Point(345, 75)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(330, 227)
+        Me.Panel1.TabIndex = 33
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.PictureBox1.Location = New System.Drawing.Point(14, 14)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(304, 193)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'txtcod
+        '
+        Me.txtcod.Enabled = False
+        Me.txtcod.Location = New System.Drawing.Point(188, 72)
+        Me.txtcod.Name = "txtcod"
+        Me.txtcod.Size = New System.Drawing.Size(121, 20)
+        Me.txtcod.TabIndex = 32
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(125, 75)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(40, 13)
+        Me.Label6.TabIndex = 31
+        Me.Label6.Text = "Codigo"
+        '
+        'btndel
+        '
+        Me.btndel.Location = New System.Drawing.Point(188, 356)
+        Me.btndel.Name = "btndel"
+        Me.btndel.Size = New System.Drawing.Size(75, 23)
+        Me.btndel.TabIndex = 30
+        Me.btndel.Text = "Eliminar"
+        Me.btndel.UseVisualStyleBackColor = True
+        '
+        'btnsave
+        '
+        Me.btnsave.Location = New System.Drawing.Point(128, 318)
+        Me.btnsave.Name = "btnsave"
+        Me.btnsave.Size = New System.Drawing.Size(75, 23)
+        Me.btnsave.TabIndex = 29
+        Me.btnsave.Text = "Guardar"
+        Me.btnsave.UseVisualStyleBackColor = True
+        '
+        'txtTelefono
+        '
+        Me.txtTelefono.Location = New System.Drawing.Point(188, 262)
+        Me.txtTelefono.Name = "txtTelefono"
+        Me.txtTelefono.Size = New System.Drawing.Size(121, 20)
+        Me.txtTelefono.TabIndex = 28
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(125, 265)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(49, 13)
+        Me.Label5.TabIndex = 27
+        Me.Label5.Text = "Teléfono"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(125, 231)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(32, 13)
+        Me.Label4.TabIndex = 26
+        Me.Label4.Text = "Email"
+        '
+        'txtCAI
+        '
+        Me.txtCAI.Location = New System.Drawing.Point(188, 185)
+        Me.txtCAI.Name = "txtCAI"
+        Me.txtCAI.Size = New System.Drawing.Size(121, 20)
+        Me.txtCAI.TabIndex = 25
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(125, 188)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(24, 13)
+        Me.Label3.TabIndex = 24
+        Me.Label3.Text = "CAI"
+        '
+        'txtRTN
+        '
+        Me.txtRTN.Location = New System.Drawing.Point(188, 146)
+        Me.txtRTN.Name = "txtRTN"
+        Me.txtRTN.Size = New System.Drawing.Size(121, 20)
+        Me.txtRTN.TabIndex = 23
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(125, 149)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(30, 13)
+        Me.Label2.TabIndex = 22
+        Me.Label2.Text = "RTN"
+        '
+        'btncan
+        '
+        Me.btncan.Location = New System.Drawing.Point(234, 318)
+        Me.btncan.Name = "btncan"
+        Me.btncan.Size = New System.Drawing.Size(75, 23)
+        Me.btncan.TabIndex = 20
+        Me.btncan.Text = "Cancelar"
+        Me.btncan.UseVisualStyleBackColor = True
+        '
+        'txtDireccion
+        '
+        Me.txtDireccion.Location = New System.Drawing.Point(188, 109)
+        Me.txtDireccion.Name = "txtDireccion"
+        Me.txtDireccion.Size = New System.Drawing.Size(121, 20)
+        Me.txtDireccion.TabIndex = 19
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(125, 112)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(52, 13)
+        Me.Label1.TabIndex = 18
+        Me.Label1.Text = "Dirección"
+        '
+        'TxtEmail
+        '
+        Me.TxtEmail.Location = New System.Drawing.Point(188, 228)
+        Me.TxtEmail.Name = "TxtEmail"
+        Me.TxtEmail.Size = New System.Drawing.Size(121, 20)
+        Me.TxtEmail.TabIndex = 35
+        '
+        'MiEmpresa
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TxtEmail)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.txtcod)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.btndel)
+        Me.Controls.Add(Me.btnsave)
+        Me.Controls.Add(Me.txtTelefono)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtCAI)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtRTN)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btncan)
+        Me.Controls.Add(Me.txtDireccion)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "MiEmpresa"
+        Me.Text = "MiEmpresa"
+        Me.Panel1.ResumeLayout(False)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents txtcod As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents btndel As Button
+    Friend WithEvents btnsave As Button
+    Friend WithEvents txtTelefono As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtCAI As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtRTN As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btncan As Button
+    Friend WithEvents txtDireccion As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TxtEmail As TextBox
+End Class
---
+++ BazarRoxana/MiEmpresa.vb
@@ -0,0 +1,5 @@
+﻿Public Class MiEmpresa
+    Private Sub btnsave_Click(sender As Object, e As EventArgs) Handles btnsave.Click
+
+    End Sub
+End Class
