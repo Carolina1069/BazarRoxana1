809c82710ff15932b296650a907de708d9a74f66
Carlos Amaya <carlosdaniel1899@gmail.com>
2021-07-27 10:13:02 -0600

    Minimo

--- BazarRoxana/Minimo.Designer.vb
+++ BazarRoxana/Minimo.Designer.vb
@@ -20,34 +20,78 @@ Partial Class Minimo
     'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
     'Se puede modificar usando el Diseñador de Windows Forms.  
     'No lo modifique con el editor de código.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.DGV = New System.Windows.Forms.DataGridView()
-        CType(Me.DGV, System.ComponentModel.ISupportInitialize).BeginInit()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle2 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle3 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle4 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.DgvMinimo = New System.Windows.Forms.DataGridView()
+        CType(Me.DgvMinimo, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'DGV
+        'DgvMinimo
         '
-        Me.DGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DGV.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.DGV.Location = New System.Drawing.Point(0, 0)
-        Me.DGV.Name = "DGV"
-        Me.DGV.Size = New System.Drawing.Size(934, 650)
-        Me.DGV.TabIndex = 0
+        Me.DgvMinimo.AllowUserToAddRows = False
+        Me.DgvMinimo.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.DgvMinimo.BackgroundColor = System.Drawing.SystemColors.ScrollBar
+        Me.DgvMinimo.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.DgvMinimo.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None
+        DataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle1.BackColor = System.Drawing.Color.Gray
+        DataGridViewCellStyle1.Font = New System.Drawing.Font("Candara", 12.0!, System.Drawing.FontStyle.Bold)
+        DataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText
+        DataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.DarkGray
+        DataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.DgvMinimo.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle1
+        Me.DgvMinimo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window
+        DataGridViewCellStyle2.Font = New System.Drawing.Font("Candara", 12.0!, System.Drawing.FontStyle.Bold)
+        DataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText
+        DataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.ControlDark
+        DataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.[False]
+        Me.DgvMinimo.DefaultCellStyle = DataGridViewCellStyle2
+        Me.DgvMinimo.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DgvMinimo.EnableHeadersVisualStyles = False
+        Me.DgvMinimo.GridColor = System.Drawing.SystemColors.ControlLight
+        Me.DgvMinimo.Location = New System.Drawing.Point(0, 0)
+        Me.DgvMinimo.Name = "DgvMinimo"
+        Me.DgvMinimo.ReadOnly = True
+        Me.DgvMinimo.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.[Single]
+        DataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle3.BackColor = System.Drawing.Color.Gray
+        DataGridViewCellStyle3.Font = New System.Drawing.Font("Candara", 12.0!, System.Drawing.FontStyle.Bold)
+        DataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText
+        DataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.DarkGray
+        DataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.DgvMinimo.RowHeadersDefaultCellStyle = DataGridViewCellStyle3
+        Me.DgvMinimo.RowHeadersWidth = 51
+        DataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.ScrollBar
+        DataGridViewCellStyle4.ForeColor = System.Drawing.Color.Black
+        DataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.DarkGray
+        DataGridViewCellStyle4.SelectionForeColor = System.Drawing.Color.White
+        Me.DgvMinimo.RowsDefaultCellStyle = DataGridViewCellStyle4
+        Me.DgvMinimo.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.DgvMinimo.Size = New System.Drawing.Size(1040, 640)
+        Me.DgvMinimo.TabIndex = 72
         '
         'Minimo
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(934, 650)
-        Me.Controls.Add(Me.DGV)
+        Me.ClientSize = New System.Drawing.Size(1040, 640)
+        Me.Controls.Add(Me.DgvMinimo)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "Minimo"
         Me.Text = "Minimo"
-        CType(Me.DGV, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DgvMinimo, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
-    Friend WithEvents DGV As DataGridView
+    Friend WithEvents DgvMinimo As DataGridView
 End Class
--- BazarRoxana/Minimo.vb
+++ BazarRoxana/Minimo.vb
@@ -2,12 +2,12 @@
 Public Class Minimo
     Private Sub Minimo_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         AbrirConeccion()
-        Dim DatosCat As New DataTable 'tabla temporal que recoge los datos de la consulta
-        Using adaptador As New SqlDataAdapter("select CodProduc as 'Codigo del Producto', NombProduc as 'Nombre del Producto', PrimerPrecio as 'Precio #1', SegundoPrecio as 'Precio #2', TercerPrecio as 'Precio #3', UnidadesStock 'Unidades en Stock', Minimo as 'Unidades Minimas' from Producto where UnidadesStock <= Minimo", ConexionBase)
-            adaptador.Fill(DatosCat)
-        End Using 'intermediario entre la base de datos y DATOSusuario para poder ingresar a datatable
+        Dim DatosMinimo As New DataTable 'tabla temporal que recoge los datos de la consulta
+        Using Adaptador As New SqlDataAdapter("select CodProduc as 'Código del producto', NombProduc as 'Nombre del producto',DescripProduc as 'Artículo',Descripcion as 'Descripción', PrimerPrecio as 'Precio #1', SegundoPrecio as 'Precio #2', TercerPrecio as 'Precio #3', UnidadesStock 'Unidades en stock', Minimo as 'Unidades minimas' from Producto where UnidadesStock <= Minimo", ConexionBase)
+            Adaptador.Fill(DatosMinimo)
+        End Using 'intermediario eDatosCatntre la base de datos y DATOSusuario para poder ingresar a datatable
 
-        DGV.DataSource = DatosCat
+        DgvMinimo.DataSource = DatosMinimo
 
         ConexionBase.Close()
     End Sub
